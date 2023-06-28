6aefea913f98ce87274e6e231349b961e4e76bcf
amankashyap004 <amankashyap004@gmail.com>
2023-06-28 17:42:59 +0530

    install tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-   return <main>app</main>;
+   return (
+      <main>
+         <h1 className="text-3xl font-bold text-red-500">Hello world!</h1>
+      </main>
+   );
 }
 
 export default App;
